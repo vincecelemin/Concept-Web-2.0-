a9068becb3dd37bb3bf11f20e8918268cfc729ce
Vince Celemin <vincecelemin@gmail.com>
2018-05-26 00:34:44 +0800

    added customer carts table

---
+++ database/migrations/2018_05_25_162718_create_customer_carts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_carts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->addColumn('integer', 'customer_profile_id', ['unsigned' => true, 'length' => 10]);
+            $table->addColumn('integer', 'product_id', ['unsigned' => true, 'length' => 10]);
+            $table->addColumn('integer', 'quantity', ['length'=>10]);
+
+            $table->index(['customer_profile_id', 'product_id']);
+            $table->foreign('customer_profile_id')->references('id')->on('customer_profiles');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_carts');
+    }
+}
