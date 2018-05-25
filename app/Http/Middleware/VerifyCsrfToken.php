704623a85e96cd969703f724f6a88ba18a9414ac
Vince Celemin <vincecelemin@gmail.com>
2018-05-26 00:25:51 +0800

    modified product cards on index

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'http://10.0.2.2/*',
     ];
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
         <div class="col-12 text-right p-0">
             <div class="list-group" id="list-tab" role="tablist">
-                <a class="list-group-item list-group-item-action active" data-toggle="list" href="#list-profile">Products</a>
+                <a class="list-group-item list-group-item-action active" data-toggle="list" href="#home-product-container">Products</a>
                 <a class="list-group-item list-group-item-action" data-toggle="list" href="#list-profile">Orders</a>
                 <a class="list-group-item list-group-item-action" data-toggle="list" href="#list-profile">Sales</a>
             </div>
@@ -26,16 +26,21 @@
                 <h4 class="mt-5">Your product list seems to be empty. You can add some by clicking <a href="products/create" class="text-dark">here!</a></h4>
 
             @else
-                @foreach($products as $product)
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div id="home-product-container">
+                    @foreach($products as $product)
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{asset('storage/product_images/'.$product->pictures->first()->image_location)}}" alt="{{$product->name}}" style="width: 250px; height: 250px;">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>{{$product->name}}</b></h5>
+                            <p class="card-text">{{$product->description}}</p>
+                            <div class="text-right">
+                                <a href="/products/{{$product->id}}/edit" class="btn btn-outline-dark">Edit</a>
+                                <a href="/products/{{$product->id}}" class="btn btn-outline-dark">View</a>
+                            </div>
+                        </div>
                     </div>
+                    @endforeach
                 </div>
-                @endforeach
             @endif
         </div>
         
